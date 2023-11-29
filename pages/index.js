6c3568d326680d2bb9ce227374716a55fada0935
Rodrigo Inácio <rodrigoinco@gmail.com>
2023-11-29 02:04:58 +0000

    add home

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Fer, eu te amo!<br/> Saiba que mesmo na correria do dia a dia, estou sempre pensando em nós!<br/> Obrigado pelo maravilho aprendizado nessa jornada com nosso pequeno, vocês são minha vida! <span style={{color:'red'}}>S2</span></h1>
+}
+
+export default Home
