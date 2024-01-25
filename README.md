badec433d4ca363dbea8ca983f4b0e97cbf54e37
Rodrigo Inácio <rodrigoinco@gmail.com>
2024-01-25 21:56:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Repositório criado para implementar o https://tabnews.com.br fazendo parte dos estudos de programação do https://curso.dev 
+
+Repositório criado para implementar o https://tabnews.com.br fazendo parte dos estudos de programação do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Fer, eu te amo!<br/> Saiba que mesmo na correria do dia a dia, estou sempre pensando em nós!<br/> Obrigado pelo maravilho aprendizado nessa jornada com nosso pequeno, vocês são minha vida! <span style={{color:'red'}}>S2</span></h1>
+function Home() {
+  return (
+    <h1>
+      Fer, eu te amo!
+      <br /> Saiba que mesmo na correria do dia a dia, estou sempre pensando em
+      nós!
+      <br /> Obrigado pelo maravilho aprendizado nessa jornada com nosso
+      pequeno, vocês são minha vida! <span style={{ color: "red" }}>S2</span>
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
