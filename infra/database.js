97333490fc0a3a4699ea607625ce52dd3da094ce
Rodrigo Inácio <rodrigoinco@gmail.com>
2024-09-05 19:19:57 +0000

    removing CA verify

--- infra/database.js
+++ infra/database.js
@@ -35,11 +35,11 @@ export default {
 };
 
 function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    return {
-      ca: process.env.POSTGRES_CA,
-    };
-  }
+  // if (process.env.POSTGRES_CA) {
+  //   return {
+  //     ca: process.env.POSTGRES_CA,
+  //   };
+  // }
 
   return process.env.NODE_ENV === "production" ? true : false;
 }
