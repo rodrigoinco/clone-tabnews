8b9f4c7efabe7279b522361f681950537489588f
Rodrigo Inácio <rodrigoinco@gmail.com>
2024-09-05 12:50:25 +0000

    adds `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOption = await {
+    dbClient: dbClient,
+    dryRun: true,
+    verbose: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendentMigrations = await migrationRunner(defaultMigrationsOption);
+    dbClient.end();
+    return response.status(200).json(pendentMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOption,
+      dryRun: false,
+    });
+
+    dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
