d8a0934d2ac6b38f93d32863555ead233fe7ffbb
Rodrigo Inácio <rodrigoinco@gmail.com>
2025-08-22 14:03:08 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orchestration.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonimous user", () => {
+    test("Retrieving the current status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toBe(parsedUpdatedAt);
 
-  const resultDatabase = responseBody.dependencies.database;
-  expect(resultDatabase.version).toEqual("16.0");
-  expect(resultDatabase.max_connections).toBe(100);
-  expect(resultDatabase.opened_connections).toEqual(1);
+      const resultDatabase = responseBody.dependencies.database;
+      expect(resultDatabase.version).toEqual("16.0");
+      expect(resultDatabase.max_connections).toBe(100);
+      expect(resultDatabase.opened_connections).toEqual(1);
+    });
+  });
 });
